f92dc3668cef68a68d1144d5181029b8b0016cbf
JackMarelli <marelligiacomo@gmail.com>
2024-10-17 13:45:29 +0200

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          DEFAULT: "#ffffff",
+        },
+        gray: {
+          DEFAULT: "#777777",
+          "333": "333333",
+        },
+        dark: {
+          DEFAULT: "#000000",
+        },
+        accent: {
+          DEFAULT: "#ff2ce3",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
