ef3184fc297067b170b635159005eb18c62ebf81
JackMarelli <marelligiacomo@gmail.com>
2024-12-20 11:21:51 +0100

    bonny sta urlando

--- src/routes/Map/Map.jsx
+++ src/routes/Map/Map.jsx
@@ -6,6 +6,7 @@ import GridLayout from "../../layouts/GridLayout/GridLayout";
 import SectionHeader from "../../components/SectionHeader/SectionHeader";
 import TextScramble from "../../components/TextScramble/TextScramble";
 import ApiManager from "../../api/ApiManager/ApiManager";
+import { log } from "three/webgpu";
 
 const Map = () => {
   const [inputValue, setInputValue] = useState("");
@@ -14,43 +15,42 @@ const Map = () => {
 
   const handleInputChange = (e) => {
     const value = e.target.value;
-    const regex = /^\d{0,6}(\.\d{0,2})?$/; // Allows XXXXXX.XX format
+    const regex = /^-?(1[0-7][0-9]|[1-9]?[0-9]|180)(\.\d{0,6})?$/;
+
+    // Ensure valid input or empty string
     if (regex.test(value) || value === "") {
       setInputValue(value);
     }
 
-    let valueString = value.toString();
-    console.log(valueString);
-
-    let len = valueString.length;
-
-    if (len >= 9) {
-      e.target.value = valueString.slice(0, 7);
+    // Handle exceeding max length
+    if (value.length >= 9) {
+      e.target.value = value.slice(0, 8);
+      console.log("logitude:", value);
 
       api
-        .post("check-longitude", { code: valueString })
+        .post("check-longitude", { longitude: value })
         .then((response) => {
           if (response.data?.message) {
+            console.log("CORRETTO");
             e.target.classList.add("!text-green-500");
             inputBoxRef.current.classList.add("!bg-green-500");
             setTimeout(() => {
               inputBoxRef.current.classList.remove("!bg-green-500");
               e.target.classList.remove("!text-green-500");
-              e.target.value = "";
+              setInputValue("");
             }, 500);
           } else {
+            console.log("ERRATO");
             e.target.classList.add("!text-red-500");
             inputBoxRef.current.classList.add("!bg-red-500");
             setTimeout(() => {
               inputBoxRef.current.classList.remove("!bg-red-500");
               e.target.classList.remove("!text-red-500");
-              e.target.value = "";
+              setInputValue("");
             }, 500);
           }
         })
-        .catch((error) => {
-          console.error(error);
-        });
+        .catch((error) => console.error(error));
     }
   };
 
@@ -61,18 +61,19 @@ const Map = () => {
           <MapBox className="absolute h-screen start-0 end-0" />
           <div className="absolute top-0 bottom-0 start-0 end-0 overflow-auto flex flex-col gap-x-2 px-4 py-8 bg-transparent pointer-events-none">
             <div className="my-crop w-full relative bg-white h-16 pointer-events-auto">
-              <div className="my-crop bg-bg absolute top-1/2 -translate-y-1/2 start-1/2 -translate-x-1/2 w-[calc(100%-3px)] h-[calc(100%-3px)] flex px-4 z-10 select-none">
+            <div className="my-crop font-mono bg-bg absolute top-1/2 -translate-y-1/2 start-1/2 -translate-x-1/2 w-[calc(100%-3px)] h-[calc(100%-3px)] flex px-4 z-10 select-none">
                 <input
                   ref={inputBoxRef}
-                  className="text-xl text-start bg-bg w-1/2 placeholder:text-gray-400 select-none"
+                  className="text-xl text-start w-full bg-bg placeholder:text-gray-400 select-none"
                   type="text"
                   value={inputValue}
-                  placeholder="XXXXXX.XX"
+                  placeholder="XX.XXXXX"
                   onChange={handleInputChange}
                   maxLength={9}
                 />
-                <span className="text-xl flex items-center w-1/2 font-mono ml-2 bg-bg">
-                  - 204815.55
+                <span className="text-xl flex items-center bg-bg mx-3">-</span>
+                <span className="text-xl flex grow items-center justify-end bg-bg">
+                  20.48155
                 </span>
               </div>
             </div>
@@ -106,21 +107,22 @@ const Map = () => {
         </div>
       ) : (
         <div className="w-screen h-screen flex">
-          <div className="w-1/2 overflow-auto flex flex-col gap-x-2 gap-x-4 px-8">
+          <div className="w-1/2 overflow-auto flex flex-col gap-x-2 gap-x-4 px-8 my-4">
             <SectionHeader content="Sii Intelligente" />
             <div className="my-crop w-full relative bg-white h-16 pointer-events-auto">
-              <div className="my-crop bg-bg absolute top-1/2 -translate-y-1/2 start-1/2 -translate-x-1/2 w-[calc(100%-3px)] h-[calc(100%-3px)] flex px-4 z-10 select-none">
+              <div className="my-crop font-mono bg-bg absolute top-1/2 -translate-y-1/2 start-1/2 -translate-x-1/2 w-[calc(100%-3px)] h-[calc(100%-3px)] flex px-4 z-10 select-none">
                 <input
                   ref={inputBoxRef}
-                  className="text-xl text-start bg-bg w-1/2 placeholder:text-gray-400 select-none"
+                  className="text-xl text-start w-full bg-bg placeholder:text-gray-400 select-none"
                   type="text"
                   value={inputValue}
-                  placeholder="XXXXXX.XX"
+                  placeholder="XX.XXXXX"
                   onChange={handleInputChange}
                   maxLength={9}
                 />
-                <span className="text-xl flex items-center w-1/2 font-mono ml-2 bg-bg">
-                  - 204815.55
+                <span className="text-xl flex items-center bg-bg mx-3">-</span>
+                <span className="text-xl flex grow items-center justify-end bg-bg">
+                  20.48155
                 </span>
               </div>
             </div>
@@ -151,7 +153,7 @@ const Map = () => {
               alt=""
             />
             <TextScramble>Ora voglio aprire questo scrigno.</TextScramble>
-            <div className="w-full flex grow justify-between items-end amt-8">
+            <div className="w-full flex grow justify-between items-end mb-8">
               <span className="text-xs text-secondary uppercase">
                 / Niente sarà più come prima.
               </span>
